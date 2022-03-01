c3d5b7d07134d6828a7deff0189f11730dd4ac53
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-03-01 10:17:26 +0000

    Updated dice_game.rb, now first to two winning rounds

--- dice_game.rb
+++ dice_game.rb
@@ -1,3 +1,6 @@
+my_counter = 0
+opponent_counter = 0
+
 while true do
     my_roll1 = rand(6) + 1
     my_roll2 = rand(6) + 1
@@ -10,14 +13,23 @@ while true do
     puts "I rolled " + my_result.to_s
     puts "My opponent rolled " + opponent_result.to_s
     
-    if my_result != opponent_result
+    if my_result == opponent_result
+        puts "No winner this round."
+    elsif my_result > opponent_result
+        puts "I win this round!"
+        my_counter = my_counter + 1
+    else
+        puts "Opponent wins this round..."
+        opponent_counter = opponent_counter + 1
+    end
+    
+    if my_counter == 2 || opponent_counter == 2
         break
     end
     
-    puts "No winner. Play another round!"
 end
 
-if my_result > opponent_result
+if my_counter == 2
     puts "I win!"
     return true
 else
