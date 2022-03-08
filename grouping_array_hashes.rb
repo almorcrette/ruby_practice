683e6aba5497a073b28a51e2e3076cc17d7cccb1
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-03-08 08:38:32 +0000

    updated grouping_array_hash to include instructions and first solution

--- grouping_array_hashes.rb
+++ grouping_array_hashes.rb
@@ -1,3 +1,7 @@
+# Instructions: 
+# Group the array of hashes below into a hash, where each key of the hash is a sport,
+# and each value of the hash is a list of names of people who play that sport.
+
 players = [
   { :name => "Sam", :sport => "tennis" },
   { :name => "Mary", :sport => "squash" },
@@ -9,9 +13,27 @@ output = players.group_by { |hash| hash[:sport] }.transform_values { |values| va
 
 p output
 
+# How I did it:
 # values currently are an array, within which are hashes, which have two keys, :name and :sport
 # I the values to by an array, with just the values of the :name key
 # Can therefore map array, such that each element (a hash), returns the value of that hash under the :name key
 # array.map { |hash| hash[:name] }
 # I need to apply this to the values of the parent hash.Can use transform_values
 
+# Slower, more manual way of doing it, may first solution
+
+=begin
+
+new_hash = {}
+
+players.each do |element|
+  new_hash[element[:sport]] = []
+end
+
+players.each do |element|
+  new_hash[element[:sport]].push(element[:name])
+end
+
+new_hash
+
+=end
