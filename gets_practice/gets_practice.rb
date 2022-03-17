f2daa21a4e40b99f7bfab825232ed83c3afc9a18
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-03-17 08:49:09 +0000

    Add explanatory comments

--- gets_practice/gets_practice.rb
+++ gets_practice/gets_practice.rb
@@ -1,17 +1,18 @@
 def load_text
-  filename = ARGV.first
-  return if filename.nil?
-  if File.exists?(filename)
-    para_n = 1
-    parsed_para = ""
-    while parsed_para
-      print "Paragraph #{para_n}: " 
-      parsed_para = gets(sep="\n\n")
-      print parsed_para
-      para_n += 1
+  filename = ARGV.first # pick up the first argument given when executing the program via command line
+  return if filename.nil? # end the process if there was no argument given
+  if File.exists?(filename) # check whether there's a file in the directory with the name of the argument given
+    para_n = 1 # start paragraph counter
+    parsed_para = "" # initalise a holding variable for each paragraph to parse, to be used to break the while loop
+    while parsed_para # while loop will break when the text parsing reaches end as gets returns nil at end, which is a falsey
+      print "Paragraph #{para_n}: " # print paragrapy number
+      ## Key note, every time gets is invoked, it then moves to the next part of the arg text, so must not invoke gets without printing
+      parsed_para = gets(sep="\n\n") # get the paragraph from the file, using two line breaks as separateor
+      print parsed_para # print the paragraph that's been gets'ed
+      para_n += 1 # add to paragraph counter
     end
   else
-    puts "Sorry, #{filename} doesn't exist."
+    puts "Sorry, #{filename} doesn't exist." # message if filename given in argument doesn't exist in directory.
   end
 end
 
