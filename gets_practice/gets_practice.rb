2bcf9f4aa53d53c4ab372770638a12c29864f0b5
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-03-17 08:40:55 +0000

    print paragraph by paragraph with paragraph counter

--- gets_practice/gets_practice.rb
+++ gets_practice/gets_practice.rb
@@ -2,8 +2,14 @@ def load_text
   filename = ARGV.first
   return if filename.nil?
   if File.exists?(filename)
-    text = gets.chomp
-    p text
+    para_n = 1
+    parsed_para = ""
+    while parsed_para
+      print "Paragraph #{para_n}: " 
+      parsed_para = gets(sep="\n\n")
+      print parsed_para
+      para_n += 1
+    end
   else
     puts "Sorry, #{filename} doesn't exist."
   end
